7529f2ad49ebb36238df53787eb8d077048ea472
evilcatkimi <evilcat2681994@gmail.com>
2016-09-09 14:12:30 +0700

    fix Agetest

--- tests/AgeTest.php
+++ tests/AgeTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class UserTest extends TestCase
+class AgeTest extends TestCase
 {
     public function testWithInvalidAgeGreaterThan100 (){
         $absolutePath = public_path() .'/img/scuti_logo.png';
